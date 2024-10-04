a6eda18c56278cdaae39dd400baf28dbec216f48
Karthik Periyasamy <karthik.vitaldesign@gmail.com>
2024-10-04 18:22:01 +0530

    added mandy technologies as copyright

--- Test/Unit/Model/Transport/PostmarkTest.php
+++ Test/Unit/Model/Transport/PostmarkTest.php
@@ -16,6 +16,7 @@
  * @package     Ripen_Postmark
  * @copyright   Copyright (c) SUMO Heavy Industries, LLC
  * @copyright   Copyright (c) Ripen, LLC
+ * @copyright   Copyright (c) Mandy Technologies Pvt Ltd
  * @notice      The Postmark logo and name are trademarks of Wildbit, LLC
  * @license     http://www.opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
